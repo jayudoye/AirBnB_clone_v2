23a850b9aa3f57877731a1877cecc498b59c1a44
jayudoye <joeludoye@gmail.com>
2024-06-26 17:17:51 -0700

    DBStorage - States and Cities

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,17 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 
+from models import storage_type
+
 Base = declarative_base()
+
 class BaseModel(Base):
     """A base class for all hbnb models"""
+
     __tablename__ = 'BaseModel'
     id = Column(String(60), unique=True, nullable=False, primary_key=True)
     created_at = Column()
@@ -38,14 +43,21 @@ class BaseModel(Base):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
         self.updated_at = datetime.now()
+        storage.new(self)
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary = self.__dict__.copy()
+        dictionary['__class__'] = self.__class__.__name__
+        
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
         return dictionary
+
+    def delete(self):
+        """Deletes an instance from storage"""
+        from models import storage
+        storage.delete(self)
