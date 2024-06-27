7fe4e19125a2a22bcf9a0af97072edbb6d67368b
jayudoye <joeludoye@gmail.com>
2024-06-26 17:32:12 -0700

    DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -1,19 +1,36 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, ForeignKey, String
 from sqlalchemy.orm import relationship
+from models import storage_type
+from .city import City
 
-
-class State(BaseModel, Base):
-    """ State class """
+if storage_type == 'db':
+    class State(BaseModel, Base):
+        """ State class """
     __tablename__ = 'states'
 
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_popoulates="cities")
+    cities = relationship('City', back_popoulates="cities, backref="state",
+                          cascade="all, delete-orphan"")
 
-    @getter
-    def cities(self):
+else:
+    class State(BaseModel):
+        """Attributes:
+        name (str): The name of the state.
+        """
+
+        name = ""
+
+        @property
+        def cities(self):
+            from models import storage
+            all_cities = list(storage.all(City).values())
+            return list(filter((lambda c: c.state_id == self.id), all_cities)
+
+        @getter
+        def cities(self):
         """
         gets the list of city instances
         """
