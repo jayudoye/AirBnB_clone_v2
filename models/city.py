5a91fb244525be2fc0ab7ed983d6a97dda200cef
jayudoye <joeludoye@gmail.com>
2024-06-26 17:23:36 -0700

    DBStorage - States and Cities

--- models/city.py
+++ models/city.py
@@ -5,13 +5,20 @@ from sqlalchemy import Column, String, ForeignKey
 from models.state import State
 from sqlalchemy.orm import relationship
 
-
-class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+if storage_type == 'db':
+    class City(BaseModel, Base):
+        """ The city class, contains state ID and name """
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
 
     state = relationship('State', back_populates="cities")
     places = relationship('Place', back_populates="cities"
-                          cascade="all, delete-orphan")
+            cascade="all, delete-orphan")
+
+else:
+    class City(BaseModel):
+        """ A blueprint for a City object """
+
+        state_id = ""
+        name = ""
