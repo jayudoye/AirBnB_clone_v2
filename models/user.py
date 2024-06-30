8185d323202d62dc4b2aa28eeca87dc9836c6a04
jayudoye <joeludoye@gmail.com>
2024-06-29 21:27:12 -0700

    AirBnB clone - MySQL DBStorage - Place

--- models/user.py
+++ models/user.py
@@ -1,18 +1,37 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
+"""Defines the User class."""
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+from models.base_model import Base, BaseModel
+from . import storage_type
 
-class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+if storage_type == 'db':
+    class User(BaseModel, Base):
+        """
+        Represents a User ORM
+        """
 
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete-orphan")
+        reviews = relationship("Review", backref="user",
+                               cascade="all, delete-orphan")
+else:
+    class User(BaseModel):
+        """Represent a User.
 
-    places = relationship('Place', back_populates="users",
-                          cascade='all, delete-orphan')
+        Public class attributes:
+            email (str): The email of the user.
+            password (str): The password of the user.
+            first_name (str): The first name of the user.
+            last_name (str): The last name of the user.
+        """
+        email = ""
+        first_name = ""
+        last_name = ""
+        password = ""
