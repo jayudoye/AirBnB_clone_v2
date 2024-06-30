fd9512c7c6ddc1eabb30f98827cd2ffa883df960
jayudoye <joeludoye@gmail.com>
2024-06-29 21:49:50 -0700

    AirBnB clone - MySQL Delete object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+"""Defines the FileStorage class."""
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """Represent an abstracted storage engine.
+
+    Attributes:
+        __file_path (str):  string - path to the JSON file (ex: file.json)
+        __objects (dict): dictionary - empty but will store all objects by
+        class.
+    """
     __file_path = 'file.json'
     __objects = {}
 
     def all(self, cls=None):
-        """Returns a list of models of one type of class """
+        """Returns the dictionary __objects."""
         if cls:
             key = '{}.'.format(cls.__name__)
             return {k: v for k, v in self.__objects.items() if
@@ -17,19 +23,19 @@ class FileStorage:
         return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """Set in __objects obj with key <obj_class_name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         self.__objects[key] = obj
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """Serialize __objects to the JSON file __file_path."""
         data = {k: obj.to_dict() for k, obj in
                 self.__objects.items()}
         with open(self.__file_path, "w") as f:
             json.dump(data, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -37,7 +43,6 @@ class FileStorage:
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
-
         try:
             with open(self.__file_path) as f:
                 data = f.read()
@@ -49,9 +54,9 @@ class FileStorage:
                     self.new(eval(cls_name)(**o))
         except FileNotFoundError:
             return
-    
+
     def delete(self, obj=None):
-        """ deletes an object """
+        """Deletes an object from the storage"""
         if obj is None:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
