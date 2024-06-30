bd52ecf20e8153aa72357403076315191fc1a8cc
jayudoye <joeludoye@gmail.com>
2024-06-29 21:32:19 -0700

    AirBnB clone - MySQL DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
-""" Review module for the HBNB project """
-from models.base_model import BaseModel
+"""Defines the Review class."""
+from sqlalchemy import Column, String, ForeignKey
 
+from models.base_model import Base, BaseModel
+from models import storage_type
 
-class Review(BaseModel):
-    """ Review classto store review information """
-    place_id = ""
-    user_id = ""
-    text = ""
+if storage_type == 'db':
+    class Review(BaseModel, Base):
+        """
+        Review ORM
+        """
+        __tablename__ = 'reviews'
+
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+else:
+    class Review(BaseModel):
+        """Represent a review.
+
+        Public attributes:
+            place_id (str): The Place id.
+            user_id (str): The User id.
+            text (str): The text of the review.
+        """
+
+        place_id = ""
+        user_id = ""
+        text = ""
