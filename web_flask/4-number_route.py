6b070a357909bb479bddc0664dc3d74f87396a56
jayudoye <joeludoye@gmail.com>
2024-08-01 00:02:19 +0000

    Web framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""A simple Flask server with a route that accepts an int variable"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_something(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_something(text='is cool'):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == '__main__':
+    app.run()
