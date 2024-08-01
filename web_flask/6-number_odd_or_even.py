e240ee21c715f38bcfff1b80263ee5da5853e078
jayudoye <joeludoye@gmail.com>
2024-08-01 00:20:06 +0000

    Web framework

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""A simple Flask server that displays if a number is odd or even"""
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<string:text>', strict_slashes=False)
+def c_something(text):
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<string:text>', strict_slashes=False)
+def python_something(text='is cool'):
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return "{} is a number".format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_html(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    num_type = 'odd' if n % 2 else 'even'
+    return render_template('6-number_odd_or_even.html',
+                           number=n, num_type=num_type)
+
+
+if __name__ == '__main__':
+    app.run()
